e653367fe88a792abc2c1d753542dfa00161943d
joana maria de jesus souza <98822804+JoanaMariaDeJesusSouza1@users.noreply.github.com>
2024-01-31 17:23:22 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var canvas;
+var backgroundImage, car1_img, car2_img, track;
+var fuelImage, powerCoinImage, lifeImage;
+var obstacle1Image, obstacle2Image;
+var database, gameState;
+var blastImage;
+var form, player, playerCount;
+var allPlayers, car1, car2, fuels, powerCoins, obstacles;
+var cars = [];
+var sadSound
+
+function preload() {
+  backgroundImage = loadImage("./assets/planodefundo.png");
+  car1_img = loadImage("./assets/car1.png");
+  car2_img = loadImage("./assets/car2.png");
+  track = loadImage("./assets/PISTA.png");
+  fuelImage = loadImage("./assets/fuel.png");
+  powerCoinImage = loadImage("./assets/goldCoin.png");
+  obstacle1Image = loadImage("./assets/obstacle1.png");
+  obstacle2Image = loadImage("./assets/obstacle2.png");
+  lifeImage = loadImage("./assets/life.png");
+  blastImage = loadImage("./assets/blast.png");
+  sadSound=loadSound("./assets/sad.wav");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+    
+  }
+
+  if (gameState === 1) {
+    sadSound.stop()
+    game.play();
+  }
+
+  if (gameState === 2) {
+    game.showLeaderboard();
+    sadSound.play()
+    game.end();
+    sadSound.stop()
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
